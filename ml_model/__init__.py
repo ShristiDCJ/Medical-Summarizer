737d255695f9941db6733923a86099f38b9f5097
Shristi Mishra <shristi.college.mishra@gmail.com>
2025-11-14 14:57:28 +0530

    Delete ml_model directory

--- ml_model/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Utilities for training and serving the custom transformer summarization model.
-"""
-
-from .model import TransformerModel, PositionalEncoding  # noqa: F401
-from .tokenizer_utils import load_tokenizer  # noqa: F401
-
